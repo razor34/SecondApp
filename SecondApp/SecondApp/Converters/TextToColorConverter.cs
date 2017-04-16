573967e0411fc2ede4623bf2ab8519248ab2e842
razor34 <damilis1@gmail.com>
2017-04-16 21:40:32 +0200

    Converters

---
+++ SecondApp/SecondApp/Converters/TextToColorConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SecondApp.Converters
+{
+    public class TextToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var text = value.ToString();
+                if (text.Contains("Damian"))
+                {
+                    return Color.Blue;
+                }
+            }
+            return Color.Aqua;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
